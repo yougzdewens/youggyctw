443f05e14513fb3a28179a28356bfdfefc280fda
youggydew <huguesdehen@hotmail.com>
2021-03-15 18:27:12 +0100

    add git ignore

--- ConcoursTwitter/Program.cs
+++ ConcoursTwitter/Program.cs
@@ -15,19 +15,6 @@ namespace ConcoursTwitter
         {
             ConcoursTwitterCore concoursTwitter = new ConcoursTwitterCore();
             concoursTwitter.StartAll();
-            //_client.DefaultRequestHeaders.Clear();
-            //_client.DefaultRequestHeaders.Add("Authorization", $"Bearer {bearerToken}");
-
-
-            //var session = OAuth.AuthorizeAsync("kI5aGFtrAfGJd43jpGkVxsbMH", "7gpOSlkdWEZvUMpxq97QvND2qffdQE4TBwdmzs487Jtl59vi6H").GetAwaiter().GetResult();
-
-            //string uriauth = session.AuthorizeUri.AbsoluteUri;
-
-            //string htmlTest = GetHtmlFromPage(uriauth);
-
-            //string pin = Console.ReadLine();
-
-            //var tokens = OAuth.GetTokensAsync(session, pin).GetAwaiter().GetResult();
         }
     }
 }
